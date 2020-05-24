8fdecada4e95db4d1ed56a9773b79907f285a129
Carl Froneberger <carl.froneberger@tufts.edu>
2020-05-23 21:50:09 -0400

    basic tic tac toe game

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-      <Game>
-        </Game>
+      <Game />
     </div>
   );
 }
--- src/game/Game.js
+++ src/game/Game.js
@@ -9,7 +9,6 @@ class Game extends Component {
             userTurn: true,
             isOver: false,
             isWinner: false,
-            isLoser: false,
             // coordinates like (i, j)
             // i is down
             // j is across
@@ -19,50 +18,74 @@ class Game extends Component {
         }
     }
 
+    // Checks for a win on the most recent move
+    // i is most recent move placement for down
+    // j is most recent move placement for across
     checkWin = (i, j) => {
-        const { board } = this.state;
-        if (board[i][0] === board[i][1] && board[i][0] === board[i][2]) return true
-        if (board[0][j] === board[1][j] && board[0][j] === board[2][j]) return true
-        if (board[0][0] === board[1][1] && board[0][0] === board[2][2]) return true
-        if (board[0][2] === board[1][1] && board[0][0] === board[0][2]) return true
+        if (this.checkRow(i)) return true;
+        if (this.checkCol(j)) return true;
+        if (this.checkDiag(i, j)) return true;
         return false;
     }
 
-    setSquare = (i, j) => {
-        const { userTurn, isOver, board } = this.state;
-        if (isOver) {
-            return;
-        }
-        if (board[i][j] !== '') {
-            return;
-        }
-        if (userTurn) {
-            board[i][j] = 'X';
-            this.setState({
-                board,
-                userTurn: !userTurn,
-            }); 
-        } else {
-            board[i][j] = 'O';
-            this.setState({
-                board,
-                userTurn: !userTurn,
-            })
-        }
+    checkRow = (i) => {
+        const { board } = this.state;
+        if (board[i][0] === '') return false;
+        if (board[i][0] === board[i][1] && board[i][0] && board[i][2]) return true;
+        return false;
     }
-    
+
+    checkCol = (j) => {
+        const { board } = this.state;
+        if (board[0][j] === '') return false;
+        if (board[0][j] === board[1][j] && board[0][j] === board[2][j]) return true;
+        return false;
+    }
+
+    checkDiag = (i, j) => {
+        const { board } = this.state;
+        if (board[1][1] === '') return false;
+        if (board[0][0] === board[1][1] && board[0][0] === board[2][2]) return true
+        if (board[0][2] === board[1][1] && board[0][0] === board[0][2]) return true
+    }
+
+    doMove = (i, j) => {
+        const { userTurn, isOver, board } = this.state;
+        if (isOver) return;
+        if (board[i][j] !== '') return;
+        if (userTurn) this.setSquare(i, j, 'X');
+        else this.setSquare(i, j, 'O');
+        if (this.checkWin(i, j)) this.setState({isOver: true});
+    }
+
+    setSquare = (i, j, mark) => {
+        const { userTurn, board } = this.state;
+        board[i][j] = mark;
+        this.setState({
+            board,
+            isWinner: userTurn,
+            userTurn: !userTurn,
+        }); 
+    }
+
     render() {
+        const { isOver, isWinner } = this.state;
         return (
-            <div className="gameContainer">
-                <Place value={this.state.board[0][0]} onChosen={() =>  this.setSquare(0, 0)} />
-                <Place value={this.state.board[0][1]}  onChosen={() => this.setSquare(0, 1)} />
-                <Place value={this.state.board[0][2]}  onChosen={() => this.setSquare(0, 2)} />
-                <Place value={this.state.board[1][0]}  onChosen={() => this.setSquare(1, 0)} />
-                <Place value={this.state.board[1][1]}  onChosen={() => this.setSquare(1, 1)} />
-                <Place value={this.state.board[1][2]}  onChosen={() => this.setSquare(1, 2)} />
-                <Place value={this.state.board[2][0]}  onChosen={() => this.setSquare(2, 0)} />
-                <Place value={this.state.board[2][1]}  onChosen={() => this.setSquare(2, 1)} />
-                <Place value={this.state.board[2][2]}  onChosen={() => this.setSquare(2, 2)} />
+            <div>
+                { isOver &&
+                  <div>There is a winner { isWinner ? 'you' : 'not you lmao'}</div>
+                }
+                <div className="gameContainer">
+                    <Place value={this.state.board[0][0]} onChosen={() =>  this.doMove(0, 0)} />
+                    <Place value={this.state.board[0][1]} onChosen={() => this.doMove(0, 1)} />
+                    <Place value={this.state.board[0][2]} onChosen={() => this.doMove(0, 2)} />
+                    <Place value={this.state.board[1][0]} onChosen={() => this.doMove(1, 0)} />
+                    <Place value={this.state.board[1][1]} onChosen={() => this.doMove(1, 1)} />
+                    <Place value={this.state.board[1][2]} onChosen={() => this.doMove(1, 2)} />
+                    <Place value={this.state.board[2][0]} onChosen={() => this.doMove(2, 0)} />
+                    <Place value={this.state.board[2][1]} onChosen={() => this.doMove(2, 1)} />
+                    <Place value={this.state.board[2][2]} onChosen={() => this.doMove(2, 2)} />
+                </div>
             </div>
         );
     }
