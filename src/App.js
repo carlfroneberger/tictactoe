98d7cd0400c0dac4f4fbcd498f7cfca6cd20c3ba
Carl Froneberger <carl.froneberger@tufts.edu>
2020-05-24 22:49:11 -0400

    basic routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+
 import Home from './Home/Home';
-import './App.css';
+import Overview from './Overview/Overview';
+import Game from './PlayGame/Game';
 
 function App() {
   return (
-    <div className="container">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Link to='/'>Home</Link>
+      <Link to='/Overview'>Overview</Link>
+      <Link to='/PlayGame/3'>Play</Link>
+
+      <Switch>
+        <Route exact path='/'> <Home /> </Route>
+        <Route exact path='/Overview'> <Overview /> </Route>
+        <Route exact path='/PlayGame/:id'> <Game /> </Route>
+      </Switch>
+    
+    </BrowserRouter>
   );
 }
 
